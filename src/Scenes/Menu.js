bf920ff5cc94a70e3cf0aa9dc93878777ca2be76
Matthew Guo <mjguo@ucsc.edu>
2023-05-26 10:01:27 -0700

    Tilemap walls collide in physics

--- src/Scenes/Menu.js
+++ src/Scenes/Menu.js
@@ -8,6 +8,7 @@ class Menu extends Phaser.Scene {
         const tileset = map.addTilesetImage('colored_packed', 'tilesetImage')
         const floorLayer = map.createLayer('Floor', tileset, 0, 0)
         const wallLayer = map.createLayer('Wall', tileset, 0, 0)
+        wallLayer.setCollisionByProperty({ collides: true })
 
         this.slime = this.physics.add.sprite(32, 32, 'slime', 0)
         this.anims.create({
@@ -18,6 +19,24 @@ class Menu extends Phaser.Scene {
         })
         this.slime.play('jiggle')
         this.slime.body.setCollideWorldBounds(true)
+        this.physics.add.collider(this.slime, wallLayer)
+        this.VEL = 100
         this.cursors = this.input.keyboard.createCursorKeys()
+    }
+
+    update() {
+        this.direction = new Phaser.Math.Vector2(0)
+        if(this.cursors.left.isDown) {
+            this.direction.x = -1
+        } else if(this.cursors.right.isDown) {
+            this.direction.x = 1
+        }
+        if(this.cursors.up.isDown) {
+            this.direction.y = -1
+        } else if(this.cursors.down.isDown) {
+            this.direction.y = 1
+        }
+        this.direction.normalize()
+        this.slime.setVelocity(this.VEL * this.direction.x, this.VEL * this.direction.y)
     }
 }
